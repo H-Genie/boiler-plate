a4d29537a03421e81a7c6a0d0dde64b0c9a3b705
h-genie <ses2000hj@hanmail.net>
2021-10-09 23:20:01 +0900

    라우터 분리

--- server/index.js
+++ server/index.js
@@ -19,60 +19,7 @@ const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
 // 라우터
-app.get('/', (req, res) => res.send('Hello World!'));
-
-const { User } = require('./models/User');
-
-app.post('/signup', (req, res) => {
-    const user = new User(req.body);
-    user.save((err, userInfo) => {
-        if (err) return res.json({ success: false, err });
-        res.status(200).json({ success: true, userInfo });
-    });
-});
-
-app.post('/signin', (req, res) => {
-    // 요청된 정보가 DB에 있는지 찾음
-    User.findOne({ id: req.body.id }, (err, user) => {
-        if (!user) return res.json({ loginSuccess: false, message: '가입되어 있지 않습니다' });
-
-        // DB에 있다면 비밀번호가 맞는지 확인
-        user.comparePassword(req.body.password, (err, isMatch) => {
-            if (!isMatch) return res.json({ loginSuccess: false, message: '비밀번호가 일치하지 않습니다' })
-        })
-
-        // 비밀번호가 맞으면 토큰 생성
-        user.generateToken((err, user) => {
-            if (err) return res.status(400).send(err);
-
-            // 토큰을 쿠키에 저장
-            res.cookie("auth", user.token)
-                .status(200)
-                .json({ loginSuccess: true, userID: user._id })
-        })
-    })
-})
-
-const { auth } = require('./middleware/auth');
-app.get('/auth', auth, (req, res) => {
-    res.status(200).json({
-        _id: req.user._id,
-        isAdmin: req.user.role === 0 ? false : true,
-        isAuth: true,
-        id: req.user.idy,
-        role: req.user.role
-    })
-})
-
-app.get('/logout', auth, (req, res) => {
-    User.findOneAndUpdate({ _id: req.user._id },
-        { token: '' }
-        , (err, user) => {
-            if (err) return res.json({ success: false, err });
-            res.status(200).send({ success: true })
-        }
-    )
-})
+app.use('/', require('./routes/routes'));
 
 // 포트 실행
 const port = 3001;
